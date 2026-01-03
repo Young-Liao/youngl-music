f605f733bfd3a265136c1a8d753fe10e01ca7b83
YoungL <cliff.liao@outlook.com>
2026-01-03 15:34:55 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/core";
+import MusicPicker from "./components/music-picker.vue";
+
+const selected = ref<string | null>(null);
+
+const handleSelection = (path: string) => {
+    console.log("Chose: ", path);
+    selected.value = path;
+}
+
+</script>
+
+<template>
+    <main class="container">
+        <MusicPicker @file-selected="handleSelection" />
+    </main>
+</template>
+
+<style scoped>
+.logo.vite:hover {
+    filter: drop-shadow(0 0 2em #747bff);
+}
+
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #249b73);
+}
+
+</style>
---
+++ src/components/music-picker.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { open } from '@tauri-apps/plugin-dialog';
+
+const emit = defineEmits(['fileSelected'])
+
+async function selectMusic() {
+    const files = await open({
+        multiple: true,
+        filters: [{
+            name: 'Audio',
+            extensions: ['mp3', 'wav', "flac"]
+        }]
+    })
+    console.log("Selected files: ", files);
+    emit('fileSelected', files);
+}
+
+</script>
+
+<template>
+    <button @click="selectMusic">Choose a Song</button>
+</template>
